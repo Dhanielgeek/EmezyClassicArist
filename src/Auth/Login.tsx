a79a37af6e85a95316f526a8eb5fa2dbe2651741
Rozarioehiz <rozarioehiz@gmail.com>
2024-04-14 13:30:24 -0700

    first commit

--- src/Auth/Login.tsx
+++ src/Auth/Login.tsx
@@ -1,8 +1,87 @@
+import styled from "styled-components"
+import men from '../assets/menu.png'
+import pond from '../assets/pound.png';
+import pro from '../assets/user.png';
+import cart from '../assets/grocery-store.png';
+import arrow from '../assets/left-arrow.png'
+
+
+
+const Header =styled.header`
+width:100%;
+height:13%;
+background:beige;
+
+`
+const Heading=styled.h1`
+font-family: Rubik;
+font-size:65px;
+font-weight:750
+`
 
 
 const Login = () => {
   return (
-    <div>Login</div>
+<div className="w-full h-screen " >
+
+  <Header className="flex justify-between items-center">
+<div className="w-[10%] h-[100%] flex justify-center items-center" >
+  <img src={men} alt="" className="w-[30%] h-[30%] object-contain" />
+</div>
+<Heading className="w-[50%] h-[100%] flex justify-center items-center  font-bold text-xl">
+EMEZY
+</Heading>
+<div className="w-[18%] h-[100%] flex justify-start items-center" >
+
+<img src={pond} alt="" className="w-[30%] h-[30%] object-contain" />
+
+<img src={pro} alt="" className="w-[30%] h-[30%] object-contain" />
+
+<img src={cart} alt="" className="w-[30%] h-[30%] object-contain" />
+</div>
+  </Header>
+
+ <section className="w-[100%] h-[90vh]  flex items-center justify-center " style={{backgroundColor:'beige'}} >
+
+ <div className="w-[40%] h-[80%] flex flex-col items-center justify-around gap-5 border border-solid border[grey]" >
+
+    <div className="w-[100%] h-[10%] flex justify-start items-center" >
+       <h1 className="font-bold text-2xl text-[grey]">Sign in to your existing account</h1>
+    </div>
+
+    <div className="w-[100%] h-[40%] flex justify-between items-center flex-col" >
+
+      <div className="w-[100%] h-[42%] flex flex-col justify-center items-start gap-2" >
+        <label > Email</label>
+        <input type="text"
+        required  placeholder="email" className="w-[100%] h-[68%] border-2" style={{borderColor:"lightgray"}}/>
+      </div>
+
+      <div className="w-[100%] h-[42%] flex flex-col justify-center items-start gap-2" >
+        <label > Password</label>
+        <input type="text"  required
+         placeholder="password" className="w-[100%] h-[68%] border-2" style={{borderColor:"lightgray"}}/>
+      </div>
+    </div>
+
+    <section className="w-[100%] h-[5%]" > <h3>forgot password?</h3></section>
+
+    <div className="w-[100%] h-[10%] flex justify-center  items-center">
+      <button className="w-[20%] h-[80%] bg-[grey] hover:text-white  hover:bg-[#4c4949] cursor-pointer">
+        SIGNIN
+      </button>
+    </div>
+    <div className="w-[100%] h-[8%] flex items-center justify-start" >
+    <div className="w-[8%] h-[48%] flex justify-center items-start">
+      <img src={arrow} alt="" className="w-[48%] h-[48%] object-contain" />
+      </div>
+      <div className="w-[90%] h-[89%]  flex items-start justify-start" >
+        return to store
+      </div>
+    </div>
+ </div>
+ </section>
+</div>
   )
 }
 
--- src/Auth/Sign.tsx
+++ src/Auth/Sign.tsx
@@ -1,8 +1,123 @@
+import styled from "styled-components"
+import men from '../assets/menu.png'
+import pond from '../assets/pound.png';
+import pro from '../assets/user.png';
+import cart from '../assets/grocery-store.png';
+import notify from '../assets/bell.png'
+import { useState } from "react";
+
+
+const Header =styled.header`
+width:100%;
+height:13%;
+background:beige;
+
+`
+const Heading=styled.h1`
+font-family: Rubik;
+font-size:65px;
+font-weight:750
+`
 
 
 const Sign = () => {
+
+// const [firstname,setFirstname] =useState('')
+// const[lastname,setLastname] =useState('')
+// const[email,setEmail] =useState('')
+// const[password, setPassword] =useState('')
+
+// const handlefirstname = (e) =>{
+// const newFirstname=e.target.value
+// setFirstname (newFirstname)
+// }
+
+// const handlelastname = (e) =>{
+//   const newLastname =e.target.value
+//   setLastname (newLastname)
+// }
+
+// const handleemail = (e) =>{
+//   const newEmail =e.target.value
+//   setEmail (newEmail)
+// }
+
+// const handlepassword = (e) =>{
+//   const newPassword =e.target.value
+//   setPassword (newPassword)
+// }
+
+// const data ={firstname, lastname, email,password}
+// const Url= https://emzey.onrender.com/signup
+
   return (
-    <div>Sign</div>
+<div className="w-full h-screen ">
+
+  <Header className="flex justify-between items-center ">
+<div className="w-[10%] h-[100%] flex justify-center items-center" >
+  <img src={men} alt="" className="w-[30%] h-[30%] object-contain" />
+</div>
+<Heading className="w-[40%] h-[100%] flex justify-center items-center bg-[beige]  font-bold text-xl">
+EMEZY
+</Heading>
+<div className="w-[18%] h-[100%] flex justify-start items-center" >
+
+<img src={pond} alt="" className="w-[30%] h-[30%] object-contain" />
+
+<img src={pro} alt="" className="w-[30%] h-[30%] object-contain" />
+
+<img src={cart} alt="" className="w-[30%] h-[30%] object-contain" />
+</div>
+  </Header>
+  
+ <section className="w-[100%] h-[110vh]  flex items-center justify-center " style={{backgroundColor:'beige'}}>
+ <div className="w-[40%] h-[90%] flex flex-col items-center justify-center border border-soild border[lightgrey]  gap-5 ">
+    <div className="w-[100%] h-[8%] flex justify-start items-center" >
+      CREATE ACCOUNT
+    </div>
+
+    <div className="w-[100%] h-[20%] flex justify-start items-center rounded " style={{backgroundColor:"lightgray"}} >
+      <div className="w-[7%] h-[50%] flex justify-center items-start">
+      <img src={notify} alt="" className="w-[45%] h-[45%] object-contain" />
+      </div>
+
+
+      <div className="w-[80%] h-[100%] flex  items-center">By creating an account, you’ll be able to move through the checkout process faster, store multiple shipping addresses, view and track your orders and more.</div>
+    </div>
+
+    <div className="w-[100%] h-[62%] flex justify-between items-center flex-col">
+
+      <div className="w-[100%] h-[22%] flex flex-col justify-center items-start gap-2" >
+        <label > First name</label>
+        <input type="text" required  placeholder="First name" className="w-[100%] h-[60%] border-2" />
+      </div>
+
+      <div className="w-[100%] h-[22%] flex flex-col justify-center items-start gap-2" >
+        <label > Last name</label>
+        <input type="text" required  placeholder="Last name" className="w-[100%] h-[60%] border-2" />
+      </div>
+
+      <div className="w-[100%] h-[22%] flex flex-col justify-center items-start gap-2" >
+        <label > Email</label>
+        <input type="text" required  placeholder="email" className="w-[100%] h-[60%] border-2" />
+      </div>
+
+      <div className="w-[100%] h-[22%] flex flex-col justify-center items-start gap-2">
+        <label > Password</label>
+        <input type="text"  required
+         placeholder="password" className="w-[100%] h-[60%] border-2"/>
+      </div>
+
+    </div>
+
+    <div className="w-[100%] h-[10%] flex justify-center  items-center" >
+      <button className="w-[20%] h-[80%] bg-[grey] hover:text-white  hover:bg-[#4c4949] cursor-pointer">
+        CREATE
+      </button>
+    </div>
+ </div>
+ </section>
+</div>
   )
 }
 
