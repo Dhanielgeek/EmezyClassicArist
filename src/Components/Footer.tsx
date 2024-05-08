a007a5dc7728d16e283b75d465c0d54a9e178a1c
Agbo-Emmanuel <agboe4102@gmail.com>
2024-05-08 02:43:53 +0100

    updatelinks

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,9 +2,23 @@ import logo from '../assets/logo.svg'
 import { BsFacebook } from "react-icons/bs";
 import { BsYoutube } from "react-icons/bs";
 import { BsTwitterX } from "react-icons/bs";
+import { SiInstagram } from "react-icons/si";
 import {ScrollToAboutMe, ScrollToHome, ScrollToGallery, ScrollToSongs, ScrollToContact} from './ScrollToComponent';
 
 const Footer = () => {
+
+  const handleFbLink = ()=>{
+    window.open("https://web.facebook.com/profile.php?id=100086900109152", "blank")
+  }
+  const handleTwitterLink = ()=>{
+    window.open("https://twitter.com/Emzey999", "blank")
+  }
+  const handleYoutubeLink = ()=>{
+    window.open("https://www.youtube.com/@emzeyclassic", "blank")
+  }
+  const handleInstagramLink = ()=>{
+    window.open("https://www.instagram.com/emzey_classics?igsh=OTJ0dDdhZ3Bnb3Ns", "blank")
+  }
 
   return (
 
@@ -29,13 +43,15 @@ const Footer = () => {
         </div>
         <div className='w-[30%] h-[90%] flex flex-col gap-5 phone:w-[100%] phone:items-center '>
           <h1 className='text-4xl font-semibold phone:text-3xl'>Socials</h1>
-          <p className='text-2xl phone:text-xl cursor-pointer hover:text-[blue] '>Facebook</p>
-          <p className='text-2xl phone:text-xl cursor-pointer hover:text-[blue] '>Twitter</p>
-          <p className='text-2xl phone:text-xl cursor-pointer hover:text-[blue] '>Youtube</p>
-          <div className='w-[60%] h-[70px] flex gap-4 '>
-            <div className='w-[20%] h-[100%] cursor-pointer '><BsFacebook className='w-full h-full text-[blue] ' /></div>
-            <div className='w-[20%] h-[100%] cursor-pointer '><BsTwitterX className='w-full h-full ' /></div>
-            <div className='w-[20%] h-[100%] cursor-pointer '><BsYoutube className='w-full h-full text-[red] ' /></div>
+          <p className='text-2xl phone:text-xl cursor-pointer hover:text-[blue] ' onClick={handleFbLink} >Facebook</p>
+          <p className='text-2xl phone:text-xl cursor-pointer hover:text-[blue] ' onClick={handleTwitterLink} >Twitter</p>
+          <p className='text-2xl phone:text-xl cursor-pointer hover:text-[blue] ' onClick={handleYoutubeLink} >Youtube</p>
+          <p className='text-2xl phone:text-xl cursor-pointer hover:text-[blue] ' onClick={handleInstagramLink} >Instagram</p>
+          <div className='w-[80%] h-[70px] flex gap-4 '>
+            <div className='w-[15%] h-[100%] cursor-pointer '><BsFacebook className='w-full h-full text-[blue] ' onClick={handleFbLink} /></div>
+            <div className='w-[15%] h-[100%] cursor-pointer '><BsTwitterX className='w-full h-full ' onClick={handleTwitterLink} /></div>
+            <div className='w-[15%] h-[100%] cursor-pointer '><BsYoutube className='w-full h-full text-[red] ' onClick={handleYoutubeLink} /></div>
+            <div className='w-[15%] h-[100%] cursor-pointer '><SiInstagram className='w-full h-full text-[red] ' onClick={handleInstagramLink} /></div>
           </div>
         </div>
       </div>
